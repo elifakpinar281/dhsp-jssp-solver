fa323729dfd23f3f09bd4081f435c3c853def423
elifakpinar281 <elif.akpnr28@gmail.com>
2026-08-26 09:01:13 +0200

    add comment in startdecoder

--- src/main/java/at/fhv/solver/IStartDecoder.java
+++ src/main/java/at/fhv/solver/IStartDecoder.java
@@ -3,6 +3,7 @@ package at.fhv.solver;
 import at.fhv.model.jssp.JsspProblem;
 import at.fhv.solver.impl.tabu.MachineSequences;
 
+// Start Decoder ist recht interessant, da es einen großen Unterschied macht, wo die Suche startet :)
 public interface IStartDecoder {
     MachineSequences decode(JsspProblem problem);
 }
